49bc882fbc97cfa6b6bc0ea014c142d5bcb92441
Darserg <Darserg1212>
2024-02-15 17:00:05 +0400

    20-th task

---
+++ 19-21/Darserg's edition/20.py
@@ -0,0 +1,19 @@
+# One neep code for 20'th task USE (United State Exame, most knowed as ЕГЭ)
+def f(x, h):
+    if h == 3 and x >= 68: 
+        return 1
+    elif h == 3 and x < 68:
+        return 0
+    elif x >= 68 and h < 3:
+        return 0
+    else:
+        if h % 2 == 0:
+            return f(x + 1, h + 1) or f(x + 4, h + 1) or f(x * 5, h + 1)
+            # Winner strategy
+        else:
+            return f(x + 1, h + 1) and f(x + 4, h + 1) and f(x * 5, h + 1)
+            # Looser strategy
+ 
+for x in range(1, 68):
+    if f(x, 0) == 1:
+        print(x)
