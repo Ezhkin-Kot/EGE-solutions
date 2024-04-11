1c2e772987a64bf1758b21013dd74f8890005eab
darserg <63180230+darserg@users.noreply.github.com>
2024-04-11 14:22:59 +0400

    Update Containers.py

--- 26/Containers.py
+++ 26/Containers.py
@@ -1,11 +1,8 @@
 f = open()
 n = int(f.readline())
 allContainers = [int(x) for x in f.readlines()]
-
 allContainers = sorted(allContainers, reverse=True)
-
 array = []
-countIterations = 0
 
 while len(allContainers) > 0:
     i = allContainers.pop(0)
@@ -17,7 +14,6 @@ while len(allContainers) > 0:
     array.append(temp)
 
 maxLen = 0
-
 for i in array:
     if len(i) > maxLen:
         maxLen = len(i)
